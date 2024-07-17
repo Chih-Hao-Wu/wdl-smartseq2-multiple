58008587f5876d0d743c506d244827113db7f4b3
Daniel Wu <111389053+Chih-Hao-Wu@users.noreply.github.com>
2024-07-17 17:03:19 -0400

    Update cromwell_m107.sh

--- run/cromwell_m107.sh
+++ run/cromwell_m107.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 module load cromwell
-java -Dconfig.file=../../CROMWELL_CONFIG_MDFY -jar $CROMWELL_JAR run ../../MultiSampleSmartSeq2.wdl --inputs mouse_m107_multiple.json
+java -Dconfig.file=../CROMWELL_CONFIG_MDFY -jar $CROMWELL_JAR run ../MultiSampleSmartSeq2.wdl --inputs mouse_m107_multiple.json
